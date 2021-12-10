9b09210bfabfc4fd5077d05931a371bfdee5ab61
tenenger <88726990+tenenger@users.noreply.github.com>
2021-12-11 00:01:58 +0900

    updata

--- javascript.js
+++ javascript.js
@@ -1,1 +1,2 @@
 alert('Hello Javascript');
+/* javascript를 실행시키려면 구글크롬의 검사->console에서 사용하자*/
---
+++ string.js
@@ -0,0 +1,12 @@
+/*구글크롬 console에서 사용하세요*/
+str.length;
+문자열의 길이를 표현해준다.
+
+str.toUpperCase();
+문자열의 소문자를 대문자로 바꿔준다.
+
+'Hello World'.indexOf('o');
+문자열의 해당 문자의 인덱스를 알려준다.
+
+'Hello World'.trim();
+문자열의 양쪽 공백을 없애준다.
